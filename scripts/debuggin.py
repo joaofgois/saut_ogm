2025029f3f85e6e9e44c093aa17b31ac02aef4bf
joaofgois <joao.f.gois>
2023-05-29 22:39:42 +0100

    ray tracing duncional e invSensmodel tbm Co-authored-by: jc <jcjoao@users.noreply.github.com>

--- scripts/debuggin.py
+++ scripts/debuggin.py
@@ -4,6 +4,19 @@ from matplotlib.ticker import (AutoMinorLocator, MultipleLocator)
 from matplotlib.patches import Rectangle
 from math import floor
 
+
+
+def InvSenModel(dist_prev, dist, x_med, y_med, norm_dist):
+    if dist < norm_dist-alpha_norm/2:
+        #vazio
+        ax.add_patch(Rectangle((floor(x_med), floor(y_med)), 1, 1, facecolor = 'grey'))
+    elif dist < norm_dist+alpha_norm/2 or dist_prev < norm_dist+alpha_norm/2:
+        #dentro da parede
+        ax.add_patch(Rectangle((floor(x_med), floor(y_med)), 1, 1, facecolor = 'black'))
+    else:
+        return
+    return
+
 mapa = np.zeros((20, 20))
 
 pose = [10.5, 10.5, 0]
@@ -13,7 +26,7 @@ norm_dist = 7
 alpha_norm = 1
 
 ray = 0.0
-ray_dir = pose[2] + np.pi/3
+ray_dir = pose[2] - (np.pi/2-0.0001/2) + 0.0001/2
 if ray_dir > 2*np.pi:
     ray_dir -= 2*np.pi
 xa = pose[0]
@@ -27,16 +40,19 @@ print()
 print(a)
 print(b)
 
+xfmenosalfa=xa + (norm_dist - alpha_norm/2)*np.cos(ray_dir)
+yfmenosalfa=ya + (norm_dist - alpha_norm/2)*np.sin(ray_dir)
 
 x = np.linspace(xa,xf,100)
 y = a*x+b
-
 
 fig, ax = plt.subplots(figsize=(10, 10))
 ax.xaxis.set_major_locator(MultipleLocator(1))
 ax.yaxis.set_major_locator(MultipleLocator(1))
 ax.plot(x, y)
 ax.plot(xa, ya, marker="o", markersize=4, markeredgecolor="red", markerfacecolor="red")
+ax.plot(xfmenosalfa, yfmenosalfa, marker="o", markersize=4, markeredgecolor="green", markerfacecolor="green")
+ax.plot(xf, yf, marker="o", markersize=4, markeredgecolor="green", markerfacecolor="green")
 
 plt.grid(True)
 plt.xlim([0, 20])
@@ -45,18 +61,12 @@ if np.cos(ray_dir) > 0: xi = 1
 else : xi = 0
 if np.sin(ray_dir) > 0: yi = 1
 else : yi = 0
-ax.add_patch(Rectangle((floor(xa), floor(ya)), 1, 1, facecolor = 'grey'))
-
-
-
 
 tracing = True
 while tracing:
-    #i += 1
-    #if i == 6:
-        #tracing = False
-    if floor(xa) == floor(xf) and floor(ya) == floor(yf):
-        tracing = False
+    x_prev = xa
+    y_prev = ya
+    dist_prev = dist
     #intercepcao linha vertical
     xiv = floor(xa)
     xiv += xi
@@ -67,32 +77,36 @@ while tracing:
     xih = (yih-b)/a
     disth = np.sqrt((yih-ya)**2 + (xih-xa)**2)
     #decidir qual a celula seguinte
-    if distv > disth:
+    if disth < distv:
+        #Intersecao Horizontal
         xa = xih
         ya = yih
         dist = dist + disth
-        if xi == 0: xi = -1
-        if yi == -1: yi = 0
+        if xi == -1: xi = 0
+        if yi == 0: yi = -1
     else:
+        #Intersecao Vertical
         xa = xiv
         ya = yiv
         dist = dist + distv
-        if yi == 0: yi = -1
-        if xi == -1: xi = 0
+        if yi == -1: yi = 0
+        if xi == 0: xi = -1
     
     ax.plot(xa, ya, marker="o", markersize=4, markeredgecolor="red", markerfacecolor="red")
     print('FOUND')
 
-    #aplicar o InverseSensorModel()
-    #if xi < 1: xa_prev = int(xa)
-    #else: xa_prev = int(xa-1)
-    #if yi < 1: ya_prev = int(ya)
-    #else: ya_prev = int(ya-1)
-
     #InvSenModel(norm_dist, dist, xa_prev, ya_prev)
-    ax.add_patch(Rectangle((floor(xa), floor(ya)), 1, 1, facecolor = 'grey'))
+    x_med = (xa+x_prev)/2
+    y_med = (ya+y_prev)/2
+    
+    InvSenModel(dist_prev, dist, x_med, y_med, norm_dist)
+    #ax.add_patch(Rectangle((floor(x_med), floor(y_med)), 1, 1, facecolor = 'grey'))
+
+    if floor(xf) == floor(x_med) and floor(yf) == floor(y_med):
+        tracing = False
 
 
 
+    
 
 plt.show()
